138744543bcec518ed156ddb60a8d9ea495f7c6e
Asta David <astalav@web.de>
2017-10-05 08:28:25 +0200

    Python datei initialcommitted

---
+++ darlehen.py
@@ -0,0 +1,40 @@
+from decimal import *
+import locale
+
+def startupPrint(size):
+	startEndStars = 2 * "* "
+	
+	if size < 16:
+		size = 16
+
+	line1n7 = startEndStars + size * "* " + startEndStars
+	line2n6 = startEndStars + size * "  " + startEndStars
+
+	line3   = startEndStars + ("{:^" + str(size*2) + "}").format("Willkommen zum Darlehensrechner ") + startEndStars
+	line4   = startEndStars + ("{:^" + str(size*2) + "}").format("Version: 0.1 vom 07.09.2017     ") + startEndStars
+	line5   = startEndStars + ("{:^" + str(size*2) + "}").format("Fehler bitte an david@tbs1.de   ") + startEndStars
+
+	print(line1n7)
+	print(line2n6)
+	print(line3)
+	print(line4)
+	print(line5)
+	print(line2n6)
+	print(line1n7)
+
+def calcEndfähigesDarlehenGesamt(Leihbetrag, Jahre, ZinsAsFloat):
+	return (Leihbetrag + calcEndfähigesDarlehenZins(Leihbetrag, Jahre, ZinsAsFloat))
+
+def calcEndfähigesDarlehenZins(Leihbetrag, Jahre, ZinsAsFloat):
+	return (Jahre * (Leihbetrag * ZinsAsFloat))
+
+def darlehensrechner():
+	locale.setlocale(locale.LC_ALL, '')
+	startupPrint(50)
+	leihbetrag = Decimal(input("Bitte Leihbetrag eingeben: "))
+	zins = Decimal(input("Bitte Zinssatz in % eingeben: "))/Decimal(100)
+	laufzeit = Decimal(input("Bitte Laufzeit in Jahren eingeben "))
+	print(locale.currency(calcEndfähigesDarlehenGesamt(leihbetrag,laufzeit,zins), grouping=True))
+
+
+darlehensrechner()
