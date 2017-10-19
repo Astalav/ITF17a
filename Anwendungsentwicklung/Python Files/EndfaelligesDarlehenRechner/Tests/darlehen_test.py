8fd6ed77cc4851b270509a41ae6b433efe75a0a2
Asta David <astalav@web.de>
2017-10-19 11:38:53 +0200

    Sublime Workspace+Projekt eingerichtet

--- Anwendungsentwicklung/Python Files/EndfaelligesDarlehenRechner/Tests/darlehen_test.py
+++ Anwendungsentwicklung/Python Files/EndfaelligesDarlehenRechner/Tests/darlehen_test.py
@@ -2,5 +2,5 @@ import os
 os.sys.path.append(os.path.realpath(os.path.dirname(__file__)+ "/.."))
 from Program import darlehen
 
-def test():
-	assert 2 == darlehen.calcEndfaehigesDarlehenGesamt(1, 1, 1)
+def testEnfaelligesDarlehenGesamt():
+	assert 125000 == darlehen.calcEndfaehigesDarlehenGesamt(100000, 5, 0.05)
