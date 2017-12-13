897cd09fd363749dde71814f0fd2d0ffe5f99ad2
Asta David <astalav@web.de>
2017-12-13 21:07:06 +0100

    implemented annu ausgabe

--- Anwendungsentwicklung/Python Files/AnnuitätsDarlehenRechner/Program/annuDarlehen.py
+++ Anwendungsentwicklung/Python Files/AnnuitätsDarlehenRechner/Program/annuDarlehen.py
@@ -8,37 +8,54 @@ def run():
 
 	locale.setlocale(locale.LC_ALL, '')
 
-	creditSum = getUserDecimalInput("Please enter your credit sum:", minValue=0)
+	creditSum = getUserDecimalInput("Please enter your credit sum", minValue=0)
+	if creditSum == Decimal("NaN"):
+		return
 
-	interest = getUserDecimalInput("Please enter your interest in %:", minValue=0, maxValue=10, maxAsWarning=True) # /Decimal(100)
+	interest = getUserDecimalInput("Please enter your interest in %", minValue=0, maxValue=10, maxAsWarning=True)
+	if interest == Decimal("NaN"):
+		return
+	else:
+		interest = interest/Decimal(100)
 
-	time = Decimal(input("Please enter the time your credit runs in years:"))
-	if time <= 0:
-		print("Error: Your entered time is smaller than or equal to 0. Please enter a valid number next time!")
+	time = getUserDecimalInput("Please enter the time your credit runs in years", minValue=0, maxValue=50, maxAsWarning=True)
+	if time == Decimal("NaN"):
 		return
 	
-	#print(locale.currency(calcEndfaehigesDarlehenGesamt(leihbetrag,laufzeit,zins), grouping=True))
+	print(locale.currency(calcAnnuDarlehen(creditSum, interest, time), grouping=True))
+	return calcAnnuDarlehen(creditSum, interest, time)
 
 
+def calcAnnuDarlehen(creditSum, interest, time):
+	annu = creditSum * ((((1+interest)**time)*interest) / (((1+interest)**time)-1))
+	return annu
 
-def getUserDecimalInput(inputString, minValue=Decimal(-Infinity), minAsWarning=False, maxValue=Decimal(Infinity), maxAsWarning=False):
-	userInputDecimal = Decimal(input(inputString+":"))
+def getUserDecimalInput(inputString, minValue=Decimal("-Infinity"), minAsWarning=False, maxValue=Decimal("Infinity"), maxAsWarning=False):
+	userInputDecimal = input(inputString+":")
+	try:
+		userInputDecimal = int(userInputDecimal)       
+	except ValueError:
+		print("Error: Your imput was not a number! Please enter a number next time!")
+		return Decimal("NaN")
 
 	if userInputDecimal <= minValue:
 		if minAsWarning:
-			print("Warning: Your input was smaller than or equal to %d!", minValue)
-			return userInputDecimal
+			print("Warning: Your input was smaller than or equal to %s!"% minValue)
+			return Decimal(userInputDecimal)
 		else:
-			print("Error: Your input was smaller than or equal to %d! Please enter a valid number next time!", minValue)
-			return Decimal(NaN)
+			print("Error: Your input was smaller than or equal to %s! Please enter a valid number next time!"% minValue)
+			return Decimal("NaN")
 
 	if userInputDecimal >= maxValue:
 		if maxAsWarning:
-			print("Warning: Your input was greater than or equal to %d!", maxValue)
-			return userInputDecimal
+			print("Warning: Your input was greater than or equal to %s!"% str(maxValue))
+			return Decimal(userInputDecimal)
 		else:
-			print("Error: Your input was greater than or equal to %d! Please enter a valid number next time!", maxValue)
+			print("Error: Your input was greater than or equal to %s! Please enter a valid number next time!"% maxValue)
 			return Decimal(NaN)
+
+	return Decimal(userInputDecimal)
+
 
 def welcomeMessage(size):
 	startEndStars = 2 * "* "
@@ -60,3 +77,5 @@ def welcomeMessage(size):
 	print(line5)
 	print(line2n6)
 	print(line1n7)
+
+run()
--- Anwendungsentwicklung/Python Files/AnnuitätsDarlehenRechner/Tests/darlehen_test.py
+++ Anwendungsentwicklung/Python Files/AnnuitätsDarlehenRechner/Tests/darlehen_test.py
@@ -1,6 +1,6 @@
 import os
 os.sys.path.append(os.path.realpath(os.path.dirname(__file__)+ "/.."))
-from Program import darlehen
+from Program import annuDarlehen
 
-def testEnfaelligesDarlehenGesamt():
-	assert 125000 == darlehen.calcEndfaehigesDarlehenGesamt(100000, 5, 0.05)
+def testAnnuDarlehen():
+	assert 21835.457140057595 == annuDarlehen.calcAnnuDarlehen(100000, 0.03, 5)
